55897ac00c958b6e8dd226b4441fd9c55b534f0c
Gaspare <Gas9220@users.noreply.github.com>
2023-04-11 15:29:02 +0200

    Generated password displayed on index.php

--- index.php
+++ index.php
@@ -2,23 +2,21 @@
 
 $password_length = $_GET["pass-lenght"];
 
-function randomValue()
-{
-    $upper_case = chr(rand(65, 90));
-    $lower_case = chr(rand(97, 122));
+function randomValue() {
+    $upper_case = chr(rand(65,90));
+    $lower_case = chr(rand(97,122));
     $number = rand(0, 9);
-    $symbols = '~!@#$*%`?[]{};<>?.,_-()';
-    $randomSymbol = $symbols[rand(0, strlen($symbols) - 1)];
+    $symbols= '~!@#$*%`?[]{};<>?.,_-()';
+    $randomSymbol = $symbols[rand(0, strlen($symbols)-1)];
     $options = [$upper_case, $lower_case, $number, $randomSymbol];
 
     return $options[rand(0, count($options) - 1)];
 }
-function generatePassword($characters)
-{
+function generatePassword($characters) {
     $password = "";
 
     while (strlen($password) < $characters) {
-        $password .= randomValue();
+       $password .= randomValue();
     }
 
     var_dump($password);
@@ -48,6 +46,8 @@ $generated_password = generatePassword($password_length);
             <label for="pass-lenght">Lunghezza password:</label>
             <input type="number" name="pass-lenght" placeholder="password characters">
         </form>
+        <h4>Your password is:</h4>
+        <p><?php echo $generated_password ?></p>
     </div>
 </body>
 
