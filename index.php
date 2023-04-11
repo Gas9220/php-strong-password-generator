a8a9a20cf15d9343d023eb0d8d559c27e4b7a7cd
Gaspare <Gas9220@users.noreply.github.com>
2023-04-11 15:00:55 +0200

    Form created

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h3>Password Generator</h3>
+        <form action="index.php" method="get">
+            <label for="pass-lenght">Lunghezza password:</label>
+            <input type="text" name="pass-lenght" placeholder="password characters">
+        </form>
+    </div>
+</body>
+
+</html>
